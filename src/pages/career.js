fecfd456f41c8658dd7cd677e68e45e1d0020df8
KakeyaTatsushi <lt.tatsu32@gmail.com>
2019-11-09 20:21:42 +0800

    modifie description nexseed

--- src/pages/career.js
+++ src/pages/career.js
@@ -17,11 +17,11 @@ class CareerPage extends React.Component {
             <div className="main-timeline8">
               <div className="timeline">
                 <span className="timeline-icon"></span>
-                <span className="year">2017年 4月 ~ 2019年 3月</span>
+                <span className="year">2019年 7月~12月(予定)</span>
                 <div className="timeline-content">
-                  <h3 className="title">香川大学 経済学部 入学</h3>
+                  <h3 className="title">Fullspeed Technologies Inc.</h3>
                   <p className="description">
-                    アメリカ留学を目指しTOEFLの勉強をしながら、ダンスと筋トレに奮励し、アニメとラップバトルで癒されていた。
+                    NexSeed卒業後、セブにオフィスを持つFullspeed Technologies Inc.で、エンジニアインターンとして働く。Webサイトの運営をしながら約1ヶ月の研修後、チームでコーポレートサイトを作成(Gatsby.js)。9月ごろから、実際のプロジェクトに参画さしていただいている(Cakephp)。分からないことが多すぎるため日々勉強。
                   </p>
                 </div>
               </div>
@@ -31,17 +31,17 @@ class CareerPage extends React.Component {
                 <div className="timeline-content">
                   <h3 className="title nexseed">NexSeedでIT留学</h3>
                   <p className="description nexseed">
-                    TOEFLで及第点を取得しアメリカ行きの切符を手にしたが、英語だけの留学は面白くないと感じ辞退。1年間大学を休学し、フィリピンで英語とプログラミングの学習ができるNexSeedに留学。チーム開発を通してプログラミングの基礎を学習(html, css, javascript, php)。プログラミングにはまってしまい英語を勉強しなくなる。
+                    TOEFLで及第点を取得しアメリカ留学の権利を得たが、英語だけの留学は面白くないと感じ辞退。1年間大学を休学し、フィリピンで英語とプログラミングの学習ができるNexSeedに留学。チーム開発を通してプログラミングの基礎を学習(html, css, javascript, php)。プログラミングにはまってしまい英語を勉強しなくなる。
                   </p>
                 </div>
               </div>
               <div className="timeline">
                 <span className="timeline-icon"></span>
-                <span className="year">2019年 7月~12月(予定)</span>
+                <span className="year">2017年 4月 ~ 2019年 3月</span>
                 <div className="timeline-content">
-                  <h3 className="title">Fullspeed Technologies Inc.</h3>
+                  <h3 className="title">香川大学 経済学部 入学</h3>
                   <p className="description">
-                    NexSeed卒業後、セブにオフィスを持つFullspeed Technologies Inc.で、エンジニアインターンとして働く。Webサイトの運営をしながら約1ヶ月の研修後、チームでコーポレートサイトを作成(Gatsby.js)。9月ごろから、実際のプロジェクトに参画さしていただいている(Cakephp)。分からないことが多すぎるため日々勉強。
+                    アメリカ留学を目指しTOEFLの勉強をしながら、ダンスと筋トレに奮励し、アニメとラップバトルで癒されていた。
                   </p>
                 </div>
               </div>
