470c88094b1a7398877a54d3be651ba57715e454
KakeyaTatsushi <lt.tatsu32@gmail.com>
2019-11-09 19:26:27 +0800

    conect form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,15 @@ class ContactPage extends React.Component {
       <Layout siteTitle={siteTitle} pageInfo={{ pageName: "contact" }}>
         <SEO title="Contact" />
         {/* <h1 className="text-center py-5">Get in Touch</h1> */}
-        <Form>
+        <Form 
+        name="contact"
+        method="POST" 
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
+
           <Form.Group controlId="formName">
             <Form.Label>Name</Form.Label>
             <Form.Control type="name" />
